a97a43beb0eb06d6902e50289782bfa6458c173b
Joel Ottosson <joel.ottosson@gmail.com>
2014-11-04 11:00:28 +0100

    Added scripts for running communication_test on salt-minions.

---
+++ communication/master_test_executor.py
@@ -0,0 +1,231 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, subprocess, sys, getopt, threading, time, shutil, urllib
+import salt.client
+import salt.utils.event
+
+#-----------------------------------------------------
+# Parse command line
+#-----------------------------------------------------
+class CommandLine:
+  def __init__(self):
+    opts, args = getopt.getopt(sys.argv[1:], "sh:", ["sync-only", "clear-only", "help"])
+    self.sync_safir=False
+    self.clear_only=False
+    self.sync_only=False
+    self.minion_command="*"
+    for k, v in opts:
+      if k=="-s":
+        self.sync_safir=True
+      elif k=="--sync-only":
+        self.sync_only=True
+      elif k=="--clear-only":
+        self.clear_only=True
+      else:
+        print("usage:")
+        print("  run tests without update safir: master_test_executor")
+        print("  update safir and run tests:     master_test_executor -s")
+        print("  clear old files, dont run test: master_test_executor --clear-only")
+        print("  sync safir, dont run test:      master_test_executor --sync-only")
+        sys.exit(1)
+
+    if len(args)>0:
+      self.minion_command=args[0]
+      for a in args[1:]:
+        self.minion_command=self.minion_command+" "+a
+#-----------------------------------------------------
+# EventHandler - handle when minions are ready
+#-----------------------------------------------------
+class EventHandler(threading.Thread):
+  def __init__(self, event_tag, num_minions):
+    threading.Thread.__init__(self)
+    self.event_tag=event_tag
+    self.received=False
+    self.num_minions=num_minions
+
+  def run(self):
+    ev=salt.utils.event.MasterEvent("/var/run/salt/master")
+    num_events=0
+    for data in ev.iter_events(tag=self.event_tag):
+      print("Got event from "+data["id"])
+      num_events=num_events+1
+      if num_events==self.num_minions:
+        break
+
+#-----------------------------------------------------
+# Execute test
+#-----------------------------------------------------
+class Executor:
+  def __init__(self):
+    #Parse command line
+    self.cmd=CommandLine()
+    self.client=salt.client.LocalClient()
+    
+    #find out which minions are alive
+    print("Check that the minions are alive...")
+    for tries in range(0,3):
+      ping_result=self.client.cmd(self.cmd.minion_command, 'test.ping')
+      self.minions=list(ping_result.keys())
+      print("   Got response from "+str(len(self.minions))+" minions.")
+      if len(self.minions)!=20:
+        print("   Try again to wake up the non-responding minions...")
+      else:
+        print("   All minions responded!")
+        break
+  
+    self.minions.sort()
+    
+    print("Continue test with " + str(len(self.minions)) + " minions")
+    for x in self.minions:
+      print("'"+x+"'")      
+
+  def clear(self):
+    print("Remove old test.py from minion")
+    self.client.cmd("os:Ubuntu", "cmd.run", ["rm -f /home/safir/test.py"], expr_form="grain")
+    self.client.cmd("os:Windows", "cmd.run", ["del c:\\Users\\safir\\test.py"], expr_form="grain")
+    
+    print("Remove old result.txt from minion")    
+    self.client.cmd("os:Ubuntu", "cmd.run", ["rm -f /home/safir/result.txt"], expr_form="grain")
+    self.client.cmd("os:Windows", "cmd.run", ["del c:\\Users\\safir\\result.txt"], expr_form="grain")
+    
+    print("Remove old result files from master")
+    shutil.rmtree("/home/safir/test_result/", ignore_errors=True)
+    if not os.path.exists("/home/safir/test_result/"):
+      os.makedirs("/home/safir/test_result/")
+    
+    print("Remove old runtime.zip")  
+    if os.path.exists("/home/safir/runtime.zip"):
+      os.remove("/home/safir/runtime.zip")
+      
+  def sync_safir(self):
+    print("Get latest SAFIR")
+    
+    subprocess.call(["wget", "-P", "/home/safir", "http://10.0.0.106:8080/safir/job/Project%20Stewart/Config=Release,label=win7-64-vs2013-build/lastSuccessfulBuild/artifact/safir/runtime/*zip*/runtime.zip", "--no-check-certificate"])
+    
+    print("  -run rsync on Linux minions")
+    linux_start_time=time.time()
+    linux_result=self.client.cmd("os:Ubuntu",
+                    "cmd.run",
+                    ["/usr/bin/rsync -a --delete rsync://10.0.0.106/stewart-ubuntu64-release /home/safir/safir"],
+                    timeout=600,
+                    expr_form="grain")
+    linux_end_time=time.time()
+    print("  ...finished after " + str(linux_end_time - linux_start_time) + " seconds")
+    
+    self.client.cmd("os:Windows", "cmd.run",
+                    ['rmdir c:\\Users\\safir\\safir /S, /Q'],
+                    expr_form="grain")
+    print("  -copy safir_runtime to Windows minions")
+    win_start_time=time.time()
+    win_res=self.client.cmd("os:Windows", "cp.get_file",
+                    ["salt://runtime.zip", "c:/Users/safir/safir/runtime.zip", "makedirs=True"],
+                    timeout=900, #15 min
+                    expr_form="grain")
+    win_end_time=time.time()
+    for k, v in win_res.iteritems():
+      print(k + " = " + v)
+    print("  ...finished after " + str(win_end_time - win_start_time) + " seconds")
+    
+    print("  -unzip files (windows)")
+    self.client.cmd('os:Windows', 'cmd.run',
+                    ['"c:\\Program Files\\7-Zip\\7z" x c:\\Users\\safir\\safir\\runtime.zip -oc:\\Users\\safir\\safir'],
+                    expr_form="grain")
+    print(" -sync Safir finished")
+        
+  def upload_test(self):
+    print("Upload new test.py to minion")
+    self.client.cmd("os:Ubuntu", "cp.get_file",
+                    ["salt://tests/test.py", "/home/safir/test.py"],
+                    expr_form="grain")
+    self.client.cmd("os:Windows", "cp.get_file",
+                    ["salt://tests/test.py", "c:/Users/safir/test.py"],
+                    expr_form="grain")
+        
+  def run_test(self):
+    print("Run test.py on minion")
+
+    node_count=str(len(self.minions))
+    
+    self.client.cmd_async("G@os:Ubuntu and "+self.cmd.minion_command,
+                    "cmd.run",
+                    ["python /home/safir/test.py --node-count "+node_count],
+                    expr_form="compound")    
+    
+    self.client.cmd_async("G@os:Windows and "+self.cmd.minion_command,
+                    "cmd.run",
+                    ["python c:\\Users\\safir\\test.py --node-count "+node_count],
+                    expr_form="compound")
+
+  def collect_result(self):
+    print("Collect result")
+    self.client.cmd("G@os:Ubuntu and "+self.cmd.minion_command,
+                    "cp.push",
+                    ["/home/safir/result.txt"],
+                    expr_form="compound")
+                    
+    self.client.cmd("G@os:Windows and "+self.cmd.minion_command,
+                    "cp.push",
+                    ["c:/Users/safir/result.txt"],
+                    expr_form="compound")
+
+    result_dir="/home/safir/test_result"
+    if os.path.exists(result_dir):    
+      shutil.rmtree(result_dir, ignore_errors=True)
+      os.makedirs(result_dir)
+        
+    for m in self.minions:
+      file_found=False
+      src_dir=os.path.join("/var/cache/salt/master/minions", m)
+      for root, folders, files in os.walk(src_dir):
+        for f in files:
+          if f.endswith("result.txt"):
+            src_file=os.path.join(root, f)
+            dst_file=os.path.join(result_dir, m+"_result.txt")
+            shutil.copyfile(src_file, dst_file)
+            os.remove(src_file)
+            file_found=True
+      if not file_found:
+        print("Found no result for "+m)
+
+    
+  def run(self):
+    self.clear()
+    
+    if self.cmd.clear_only:
+      return
+      
+    if self.cmd.sync_only:
+      self.sync_safir()
+      return
+  
+    #Start a thread that handles the event when minion has finished the test case
+    event_handler=EventHandler("com_test", len(self.minions))
+    event_handler.start()
+    
+    if self.cmd.sync_safir:
+      self.sync_safir()
+    else:
+      print("Skip rsync safir step...")
+
+    self.upload_test()    
+    self.run_test()    
+      
+    print("Wait for finished signal from the minions")
+    event_handler.join()
+
+    #If we received a test finished event then collect the result
+    self.collect_result()
+
+def main():
+  print("===== Start =====")  
+  ex=Executor()
+  ex.run()
+  print("--- Finished! ---")
+  return 0  
+  
+#------------------------------------------------
+# If this is the main module, start the program
+#------------------------------------------------
+if __name__ == "__main__":
+  sys.exit(main())
+  
---
+++ communication/test.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, subprocess, sys, getopt, time
+from socket import gethostname
+
+server_minion = "minion00"
+node_type = "nt1" #nt0 has no multicast, nt1 and nt2 is multicast enabled
+node_count = 20
+message_count = 100000 #number of messages to send from server to clients
+
+
+def ip_address(host_name):
+  ip="192.168.66.1"+host_name[len(host_name)-2:]
+  return ip
+
+def set_env(name, value):
+  if os.environ.get(name) is not None:
+    os.environ[name]+=os.pathsep+value
+  else:
+    os.environ[name]=value
+    
+def communication_test_cmd():
+  my_name=gethostname()
+  if my_name==server_minion:
+    return ["communication_test",
+            "-a", ip_address(my_name)+":10000",
+            "-t", node_type,
+            "-w", str(node_count-1),
+            "--nsend", str(message_count),
+            "--nrecv", "0",
+            "--size", "30000",
+            "--thread-count", "10"]
+  else:
+    return ["communication_test",
+            "-a", ip_address(my_name)+":10000",
+            "-s", ip_address(server_minion)+":10000",
+            "-t", node_type,
+            "-w", str(node_count-1),
+            "--nsend", "0",
+            "--nrecv", str(message_count),
+            "--size", "30000",
+            "--thread-count", "10"]
+  
+def linux_main():
+  try:
+    f=open("/home/safir/result.txt", "w")
+    set_env("HOME", "/home/safir")
+    set_env("PATH", "/home/safir/safir/runtime/bin")
+    set_env("LD_LIBRARY_PATH", "/home/safir/safir/runtime/lib")
+    set_env("SAFIR_RUNTIME", "/home/safir/safir/runtime")
+    
+    #subprocess.call(["dots_configuration_check", "-s"], stdout=f, stderr=f)
+    #subprocess.call(["communication_test", "-h"], stdout=f, stderr=f)
+    subprocess.call(communication_test_cmd(), stdout=f, stderr=f)    
+  except getopt.GetoptError as err:
+    f.write(err)
+  except:
+    f.write("Exception caught")
+    
+  f.flush()
+  f.close()
+
+def windows_main():
+  try:
+    f=open("c:\\Users\\safir\\result.txt", "w")    
+    set_env("PATH", "c:\\Users\\safir\\safir\\runtime\\bin")
+    set_env("SAFIR_RUNTIME", "c:\\Users\\safir\\safir\\runtime")
+    #subprocess.call(["dots_configuration_check", "-s"], stdout=f, stderr=f, shell=True)
+    #subprocess.call(["communication_test", "-h"], stdout=f, stderr=f)
+    subprocess.call(communication_test_cmd(), stdout=f, stderr=f, shell=True)
+  except getopt.GetoptError as err:
+    f.write(err)
+  except:
+    f.write("Exception caught")
+    
+  f.flush()
+  f.close()
+  
+def main():  
+  global node_count
+  opts, args = getopt.getopt(sys.argv[1:], "", ["node-count="])
+  for o, a in opts:
+    if o=="--node-count":
+      node_count=int(a)
+  
+  if sys.platform.lower().startswith('linux'):
+    linux_main()
+  elif sys.platform.lower().startswith('win'):
+    windows_main()
+	
+	#signal that we are done
+  subprocess.call(["salt-call", "event.fire_master", gethostname(), "com_test"])
+   
+#------------------------------------------------
+# If this is the main module, start the program
+#------------------------------------------------
+if __name__ == "__main__":
+  main()
+  
