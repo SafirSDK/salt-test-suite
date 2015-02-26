5467f1e2a38e7a5f7a8ca1da61bb0a1351029408
Lars Hagström <lars@foldspace.nu>
2015-02-26 13:26:39 +0100

    jenkinsification

--- master_test_executor.py
+++ master_test_executor.py
@@ -1,8 +1,36 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright Consoden AB, 2014-2015 (http://www.consoden.se)
+#
+# Created by: Joel Ottosson / joel.ottosson@consoden.se
+#
+###############################################################################
+#
+# This file is part of Safir SDK Core.
+#
+# Safir SDK Core is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# Safir SDK Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import print_function
 import os, subprocess, sys, getopt, threading, time, shutil, urllib, glob
 import salt.client
 import salt.utils.event
+
+def log(*args, **kwargs):
+    print(*args, **kwargs)
+    sys.stdout.flush()
 
 #-----------------------------------------------------
 # Parse command line
@@ -31,12 +59,12 @@ class CommandLine:
         self.test_script=os.path.basename(v)
 
       else:
-        print("usage:")
-        print("  run test script kalle.py: master_test_executor --test-script kalle.py")
-        print("  clear old files: master_test_executor --clear-only")
-        print("  update safir: master_test_executor --safir-update")
-        print("  collect log files: master_test_executor --get-logs")
-        print("  collect all result files: master_test_executor --get-results")
+        log("usage:")
+        log("  run test script kalle.py: master_test_executor --test-script kalle.py")
+        log("  clear old files: master_test_executor --clear-only")
+        log("  update safir: master_test_executor --safir-update")
+        log("  collect log files: master_test_executor --get-logs")
+        log("  collect all result files: master_test_executor --get-results")
         sys.exit(1)
 
     if len(args)>0:
@@ -57,7 +85,7 @@ class EventHandler(threading.Thread):
     ev=salt.utils.event.MasterEvent("/var/run/salt/master")
     num_events=0
     for data in ev.iter_events(tag=self.event_tag):
-      print("Got event from "+data["id"])
+      log("Got event from", data["id"])
       num_events=num_events+1
       if num_events==self.num_minions:
         break
@@ -69,50 +97,50 @@ class Executor:
   def __init__(self):
     #Parse command line
     self.cmd=CommandLine()
-    
+
     self.client=salt.client.LocalClient()
-    
+
     #find out which minions are alive
-    print("Check that the minions are alive...")
+    log("Check that the minions are alive...")
     for tries in range(0,3):
       ping_result=self.client.cmd(self.cmd.minion_command, 'test.ping')
       self.minions=list(ping_result.keys())
-      print("   Got response from "+str(len(self.minions))+" minions.")
+      log("   Got response from "+str(len(self.minions))+" minions.")
       if len(self.minions)!=20:
-        print("   Try again to wake up the non-responding minions...")
+        log("   Try again to wake up the non-responding minions...")
       else:
-        print("   All minions responded!")
+        log("   All minions responded!")
         break
-  
+
     self.minions.sort()
-    
-    print("Continue test with " + str(len(self.minions)) + " minions")
+
+    log("Continue test with " + str(len(self.minions)) + " minions")
     for x in self.minions:
-      print("'"+x+"'")      
+      log("'"+x+"'")
 
   def clear(self):
     if self.cmd.test_script is not None:
-      print("Remove old test script from minion")
+      log("Remove old test script from minion")
       self.client.cmd("os:Ubuntu", "cmd.run", ["rm -f /home/safir/"+self.cmd.test_script], expr_form="grain")
       self.client.cmd("os:Windows", "cmd.run", ["del c:\\Users\\safir\\"+self.cmd.test_script], expr_form="grain")
-    
-    print("Remove old result.txt from minion")    
+
+    log("Remove old result.txt from minion")
     self.client.cmd("os:Ubuntu", "cmd.run", ["rm -f /home/safir/result.txt"], expr_form="grain")
     self.client.cmd("os:Windows", "cmd.run", ["del c:\\Users\\safir\\result.txt"], expr_form="grain")
-    
-    print("Remove old result files from master")
+
+    log("Remove old result files from master")
     shutil.rmtree("/home/safir/test_result/", ignore_errors=True)
     if not os.path.exists("/home/safir/test_result/"):
       os.makedirs("/home/safir/test_result/")
-    
-    print("Remove old deb- and exe- files")
+
+    log("Remove old deb- and exe- files")
     for fl in glob.glob("/home/safir/*.deb"):
       os.remove(fl)
     for fl in glob.glob("/home/safir/*.exe"):
       os.remove(fl)
-      
+
   def get_logs(self):
-    print("Get logs")
+    log("Get logs")
     #create zip file of all logs since our version of salt cant push directories
     self.client.cmd("G@os:Ubuntu and "+self.cmd.minion_command,
                     "cmd.run",
@@ -131,16 +159,16 @@ class Executor:
                     "cp.push",
                     ["c:\\Users\\safir\\log.zip"],
                     expr_form="compound")
-    
+
     log_dir="/home/safir/log"
     if not os.path.exists(log_dir):
       os.makedirs(log_dir)
-        
+
     for m in self.minions:
       minion_log_dir=os.path.join(log_dir, m)
       if not os.path.exists(minion_log_dir):
         os.makedirs(minion_log_dir)
-        
+
       file_found=False
       src_dir=os.path.join("/var/cache/salt/master/minions", m)
       for root, folders, files in os.walk(src_dir):
@@ -152,26 +180,26 @@ class Executor:
             os.remove(src_file)
             file_found=True
       if file_found:
-        #unzip the file        
+        #unzip the file
         subprocess.call(["unzip", "-o", dst_file, "-d", minion_log_dir])
         os.remove(dst_file)
-        print(m+" - logs collected")
+        log(m+" - logs collected")
       else:
-        print(m+" - no logs found")
-                    
-    print(" -get logs finished")
-    
+        log(m+" - no logs found")
+
+    log(" -get logs finished")
+
   def download_from_jenkins(self):
     subprocess.call(["wget", "-nv", "-O", "/home/safir/deb.zip",
     "https://10.0.0.107/safir/job/Project%20Stewart/Config=Release,label=ubuntu-trusty-lts-64-build/lastSuccessfulBuild/artifact/*zip*/archive.zip", "--no-check-certificate"])
     subprocess.call(["wget", "-nv", "-O",
     "/home/safir/win.zip", "https://10.0.0.107/safir/job/Project%20Stewart/Config=Release,label=win7-64-vs2013-build/lastSuccessfulBuild/artifact/*zip*/archive.zip", "--no-check-certificate"])
-    
+
     subprocess.call(["unzip", "-o", "/home/safir/deb.zip", "-d", "/home/safir"])
     subprocess.call(["unzip", "-o", "/home/safir/win.zip", "-d", "/home/safir"])
     os.remove("/home/safir/deb.zip")
     os.remove("/home/safir/win.zip")
-    
+
     for root, folders, files in os.walk("/home/safir/archive"):
         for f in files:
           dst_file=None
@@ -183,20 +211,20 @@ class Executor:
             dst_file="/home/safir/safir-sdk-core.deb"
           elif f.startswith("SafirSDKCore"):
             dst_file="/home/safir/SafirSDKCore.exe"
-            
+
           src_file=os.path.join(root, f)
           shutil.copyfile(src_file, dst_file)
-          
+
     shutil.rmtree("/home/safir/archive", ignore_errors=True)
-                    
+
   def update_linux(self):
-    print("  -update Linux minions")
+    log("  -update Linux minions")
     linux_start_time=time.time()
-    
+
     safir_core="safir-sdk-core.deb"
     safir_test="safir-sdk-core-testsuite.deb"
     safir_dev="safir-sdk-core-dev.deb"
-    
+
     self.client.cmd("os:Ubuntu", "cp.get_file",
                     ["salt://"+safir_core, "/home/safir/"+safir_core, "makedirs=True"],
                     timeout=900, #15 min
@@ -210,7 +238,7 @@ class Executor:
                     timeout=900, #15 min
                     expr_form="grain")
 
-    print("   installing packages")
+    log("   installing packages")
     self.client.cmd('os:Ubuntu', 'cmd.run',
                     ['sudo apt-get -y purge safir-sdk-core safir-sdk-core-testsuite safir-sdk-core-dev'],
                     expr_form="grain")
@@ -219,15 +247,15 @@ class Executor:
     self.client.cmd('os:Ubuntu', 'cmd.run', ['sudo dpkg -i '+safir_dev], expr_form="grain")
 
     linux_end_time=time.time()
-    print("  ...finished after " + str(linux_end_time - linux_start_time) + " seconds")
-        
+    log("  ...finished after " + str(linux_end_time - linux_start_time) + " seconds")
+
   def windows_uninstall(self):
-    print("   uninstall old Windows installation")
+    log("   uninstall old Windows installation")
     installpath=r'c:\Program Files\Safir SDK Core'
     uninstaller = installpath + r"\Uninstall.exe"
-    
+
     self.client.cmd('os:Windows', 'cmd.run', ['"'+uninstaller+'" /S'], expr_form="grain")
-    
+
     snippet="""
 import os, time
 for x in range(0, 120):
@@ -236,75 +264,75 @@ for x in range(0, 120):
   time.sleep(5.0)
     """
     res=self.client.cmd("os:Windows", "cmd.exec_code", ["python", snippet], expr_form="grain")
-    print("   uninstall completed")
-        
+    log("   uninstall completed")
+
   def update_windows(self):
-    print("  -update Windows minions")
+    log("  -update Windows minions")
     safir_win="SafirSDKCore.exe"
     win_start_time=time.time()
 
     self.windows_uninstall()
-    
+
     self.client.cmd("os:Windows", "cp.get_file",
                     ["salt://"+safir_win, "c:/Users/safir/"+safir_win, "makedirs=True"],
                     timeout=900, #15 min
                     expr_form="grain")
-    
+
     self.client.cmd('os:Windows', 'cmd.run',
                     ['c:\\Users\\safir\\'+safir_win+' /S /NODEVELOPMENT /TESTSUITE'], expr_form="grain")
-                
+
     win_end_time=time.time()
-    print("  ...finished after " + str(win_end_time - win_start_time) + " seconds")
-    
+    log("  ...finished after " + str(win_end_time - win_start_time) + " seconds")
+
   def sync_safir(self):
-    print("Install latest Safir SDK Core")
-        
+    log("Install latest Safir SDK Core")
+
     self.download_from_jenkins()
     self.update_linux()
     self.update_windows()
-    print(" -update Safir finished")
+    log(" -update Safir finished")
 
   def upload_test(self):
-    print("Upload new test script to minion")
+    log("Upload new test script to minion")
     self.client.cmd("os:Ubuntu", "cp.get_file",
                     ["salt://salt-test-suite/"+self.cmd.test_script_path, "/home/safir/"+self.cmd.test_script],
                     expr_form="grain")
     self.client.cmd("os:Windows", "cp.get_file",
                     ["salt://salt-test-suite/"+self.cmd.test_script_path, "c:/Users/safir/"+self.cmd.test_script],
                     expr_form="grain")
-        
+
   def run_test(self):
-    print("Run test script on minion")
+    log("Run test script on minion")
 
     node_count=str(len(self.minions))
-    
+
     self.client.cmd_async("G@os:Ubuntu and "+self.cmd.minion_command,
                     "cmd.run",
                     ["python /home/safir/"+self.cmd.test_script+" --node-count "+node_count],
-                    expr_form="compound")    
-    
+                    expr_form="compound")
+
     self.client.cmd_async("G@os:Windows and "+self.cmd.minion_command,
                     "cmd.run",
                     ["python c:\\Users\\safir\\"+self.cmd.test_script+" --node-count "+node_count],
                     expr_form="compound")
 
   def collect_result(self):
-    print("Collect result")
+    log("Collect result")
     self.client.cmd("G@os:Ubuntu and "+self.cmd.minion_command,
                     "cp.push",
                     ["/home/safir/result.txt"],
                     expr_form="compound")
-                    
+
     self.client.cmd("G@os:Windows and "+self.cmd.minion_command,
                     "cp.push",
                     ["c:/Users/safir/result.txt"],
                     expr_form="compound")
 
-    result_dir="/home/safir/test_result"
-    if os.path.exists(result_dir):    
+    result_dir="test_result"
+    if os.path.exists(result_dir):
       shutil.rmtree(result_dir, ignore_errors=True)
       os.makedirs(result_dir)
-        
+
     for m in self.minions:
       file_found=False
       src_dir=os.path.join("/var/cache/salt/master/minions", m)
@@ -317,51 +345,50 @@ for x in range(0, 120):
             os.remove(src_file)
             file_found=True
       if not file_found:
-        print("Found no result for "+m)
+        log("Found no result for "+m)
 
-    
+
   def run(self):
     if self.cmd.get_logs:
       self.get_logs()
       return
-  
+
     if self.cmd.get_results:
       self.collect_result()
       return
-      
+
     #Run the test script, start clearing old scripts and results
     self.clear()
-    
+
     if self.cmd.clear_only:
       return
-      
+
     if self.cmd.update:
       self.sync_safir()
-      return    
-    
+      return
+
     #Start a thread that handles the event when minion has finished the test case
     event_handler=EventHandler(len(self.minions))
     event_handler.start()
-    
-    self.upload_test()    
-    self.run_test()    
-      
-    print("Wait for finished signal from the minions")
+
+    self.upload_test()
+    self.run_test()
+
+    log("Wait for finished signal from the minions")
     event_handler.join()
 
     #If we received a test finished event then collect the result
     self.collect_result()
 
 def main():
-  print("===== Start =====")  
+  log("===== Start =====")
   ex=Executor()
   ex.run()
-  print("--- Finished! ---")
-  return 0  
-  
+  log("--- Finished! ---")
+  return 0
+
 #------------------------------------------------
 # If this is the main module, start the program
 #------------------------------------------------
 if __name__ == "__main__":
   sys.exit(main())
-  
