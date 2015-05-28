6ba9ba6b35ef559ae2a30bbe67de77b9fe52bc4c
Lars Hagström <lars@foldspace.nu>
2015-05-28 11:03:35 +0200

    parameters

--- system_picture/test.py
+++ system_picture/test.py
@@ -26,7 +26,7 @@
 from __future__ import print_function
 import os, subprocess, sys, getopt, time, traceback, re, socket, time
 
-NODES_PER_COMPUTER = 1
+NODES_PER_COMPUTER = 2
 LINUX_ONLY = True
 COMPUTERS = 10 + (0 if LINUX_ONLY else 10)
 
