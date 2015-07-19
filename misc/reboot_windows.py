2d74a16edd52b2f3918d26f2887d003062376ede
Lars Hagström <lars@foldspace.nu>
2015-07-19 20:52:10 +0200

    better algorithm

--- misc/reboot_windows.py
+++ misc/reboot_windows.py
@@ -40,14 +40,18 @@ else:
 
 subprocess.check_output(("salt", "-N", "win", "system.reboot"))
 
-#first wait for one node to go away
+#first wait for all windows nodes to go away
+needed = set(["minion10","minion11","minion12","minion13","minion14","minion15","minion16","minion17","minion18","minion19"])
 while (True):
     output = subprocess.check_output(("salt-run","-t","20","manage.down")).decode("utf-8")
 
-    log("Looking for a rebooting windows node")
-    if output.find("minion1") != -1:
-        log ("At least one windows node is down, as expected")
+    needed = needed - output.splitlines()
+    if len(needed) == 0:
+        log("All windows nodes appear to have gone away, as expected")
         break
+    else:
+        log("Still waiting for some nodes to go away:")
+        log(needed)
     time.sleep(1)
 
 #then wait for all to come up again
