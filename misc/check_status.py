f0e68854f62029f19346991f775178cfb7acab4c
Lars Hagström <lars@foldspace.nu>
2015-09-15 16:05:05 +0200

    debug

--- misc/check_status.py
+++ misc/check_status.py
@@ -33,7 +33,7 @@ if output.find("minion") != -1:
     sys.exit(1)
 
 output = subprocess.check_output(("salt", "--out=json", "*", "cmd.run", "safir_show_config --revision")).decode("utf-8").replace("\n}",",").replace("{","")
-
+print(output)
 
 pattern = re.compile(r"Safir SDK Core Git revision: (.*)")
 
