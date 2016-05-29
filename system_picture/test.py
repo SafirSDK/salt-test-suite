d215c2bdd81002616ed8131d2bee5e08812b6534
Lars Hagström <lars@foldspace.nu>
2016-05-29 14:08:18 +0200

    less nodes

--- system_picture/test.py
+++ system_picture/test.py
@@ -26,7 +26,7 @@
 from __future__ import print_function
 import subprocess, sys, re, socket
 
-NODES_PER_COMPUTER = 10
+NODES_PER_COMPUTER = 5
 LINUX_ONLY = True
 WINDOWS_ONLY = False
 COMPUTERS = 10 + (0 if LINUX_ONLY or WINDOWS_ONLY else 10)
