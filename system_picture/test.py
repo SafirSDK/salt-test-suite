476661a90eb6227ee33bf5c2338b945a22066795
Lars Hagström <lars@foldspace.nu>
2016-06-22 10:59:03 +0200

    add four more nodes to test

--- system_picture/test.py
+++ system_picture/test.py
@@ -24,10 +24,10 @@
 #
 ###############################################################################
 from __future__ import print_function
-import subprocess, sys, re, socket, time
+import subprocess, sys, re, socket
 
 NODES_PER_COMPUTER = 1
-COMPUTERS=[0,1,2,10,11,12]
+COMPUTERS=[0,1,2,3,4,10,11,12,13,14]
 REVOLUTIONS = 10
 
 def log(*args, **kwargs):
