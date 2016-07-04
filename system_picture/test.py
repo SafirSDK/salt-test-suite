8d8cd99e7dd7f5ac9f1cd8af589bc4b1015a4394
Lars Hagström <lars@foldspace.nu>
2016-07-04 10:58:20 +0100

    use all minions in sp test

--- system_picture/test.py
+++ system_picture/test.py
@@ -27,7 +27,8 @@ from __future__ import print_function
 import subprocess, sys, re, socket
 
 NODES_PER_COMPUTER = 1
-COMPUTERS=[0,1,2,3,4,10,11,12,13,14]
+#COMPUTERS=[0,1,2,3,4,10,11,12,13,14]
+COMPUTERS=list(range(20))
 REVOLUTIONS = 3
 
 def log(*args, **kwargs):
