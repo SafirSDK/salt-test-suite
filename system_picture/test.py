9cafafc73b2911930576440af0ab250464052304
Lars Hagström <lars@foldspace.nu>
2016-09-30 13:04:27 +0200

    only linux nodes, again

--- system_picture/test.py
+++ system_picture/test.py
@@ -28,7 +28,7 @@ import subprocess, sys, re, socket, os, shutil, time
 
 NODES_PER_COMPUTER = 3
 #COMPUTERS=[0,10]
-COMPUTERS=list(range(20))
+COMPUTERS=list(range(10))
 REVOLUTIONS = 3
 
 def log(*args, **kwargs):
